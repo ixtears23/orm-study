99173ca1d0d1ecd8b275dcc5777c35af78b2e4cf
junseokoh <ixtears23@icloud.com>
2022-07-24 02:11:18 +0900

    Chapter5 - 실전 예제2

---
+++ src/main/java/junseok/snr/orm/sample/exmaple2/Order.java
@@ -0,0 +1,49 @@
+package junseok.snr.orm.sample.exmaple2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter @Setter
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column("ORDER_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    public void setMember(Member member) {
+        if (this.member != null) {
+            this.member.getOrders().remove(this);
+        }
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+}
+
+enum OrderStatus {
+    ORDER, CANCEL;
+}
---
+++ src/main/java/junseok/snr/orm/sample/exmaple2/OrderItem.java
@@ -0,0 +1,27 @@
+package junseok.snr.orm.sample.exmaple2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@Entity
+@Table(name = "ORDER_ITEM")
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    private int orderPrice;
+    private int count;
+}
