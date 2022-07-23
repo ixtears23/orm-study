b7d6f8abec6de7508d6d6386d3e8e406f6b479f2
junseokoh <ixtears23@icloud.com>
2022-07-24 00:51:40 +0900

    Chapter4 - Main 메서드 생성

---
+++ src/main/java/junseok/snr/orm/sample/chapter5/Main.java
@@ -0,0 +1,16 @@
+package junseok.snr.orm.sample.chapter5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final Member member1 = new Member("member1", "회원1");
+        final Member member2 = new Member("member2", "회원2");
+        final Team team1 = new Team("team1", "팀1");
+
+        member1.setTeam(team1);
+        member2.setTeam(team1);
+
+        final Team team = member1.getTeam();
+
+    }
+}
