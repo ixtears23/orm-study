f59f3ccfdb58d645db19dd674a62a497bc3e4825
junseokoh <ixtears23@icloud.com>
2022-07-24 22:21:34 +0900

    Chapter6 - Member Entity 생성

---
+++ src/main/java/junseok/snr/orm/sample/chapter6/Member.java
@@ -0,0 +1,22 @@
+package junseok.snr.orm.sample.chapter6;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@Entity
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
