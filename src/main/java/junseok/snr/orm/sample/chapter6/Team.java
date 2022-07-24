7519656104f3c194cccc20d75536f674d268c98c
junseokoh <ixtears23@icloud.com>
2022-07-24 22:23:56 +0900

    Chapter6 - 양방향 연관관계 생성

--- src/main/java/junseok/snr/orm/sample/chapter6/Team.java
+++ src/main/java/junseok/snr/orm/sample/chapter6/Team.java
@@ -3,10 +3,9 @@ package junseok.snr.orm.sample.chapter6;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter @Setter
 @Entity
@@ -16,4 +15,6 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
 }
